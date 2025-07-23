c952a609d1c242700ccfbe271ed20b9be445e3f7
Wangai <jahkeyjohn@gmail.com>
2025-07-23 18:29:41 +0300

    delete note

--- frontend/src/components/FetchNote.jsx
+++ frontend/src/components/FetchNote.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { useNavigate } from "react-router";
+import toast from "react-hot-toast";
 
 import FormatDate from "../utils/FormatDate";
 import Spinner from "./Spinner";
@@ -11,8 +13,21 @@ const FetchNote = () => {
   const { id } = useParams();
   const [loading, setLoading] = useState(false);
   const [note, setNote] = useState([]);
+  const navigate = useNavigate();
 
-  console.log();
+  const handleDelete = async () => {
+    const confirm = window.confirm("Are you sure you want to delete?");
+    if (!confirm) return;
+
+    try {
+      await axios.delete(`http://localhost:8000/api/notes/${id}`);
+      toast.success("Note deleted successfully");
+      navigate("/");
+    } catch (error) {
+      console.error("Delete failed:", error);
+      toast.error("Delete failed");
+    }
+  };
 
   useEffect(() => {
     setLoading(true);
@@ -43,7 +58,10 @@ const FetchNote = () => {
           <span>
             {note.createdAt && <FormatDate createDate={note.createdAt} />}
 
-            <MdDeleteOutline />
+            <MdDeleteOutline
+              onClick={handleDelete}
+              style={{ cursor: "pointer", color: "crimson" }}
+            />
           </span>
         </div>
       </main>
